0b087255936de9be7e158f1f8b920365287c35a5
Jordan Fearnley <fearnlj01@gmail.com>
2022-01-18 18:29:52 +0000

    amendments to make tail window more useful

--- src/TS/autoHomicide.ts
+++ src/TS/autoHomicide.ts
@@ -8,9 +8,11 @@ export async function main(ns : NS) : Promise<void> {
 		const homicide = globalThis['document'].querySelector('button:nth-child(8)')
 		while(true) {
 			// @ts-expect-error placeholderDescription
+			ns.clearLog()
+			ns.print(ns.heart.break() * -1)
 			homicide[Object.keys(homicide)[1]].onClick({ isTrusted : true })
 			await sleep(4005)
 		}
 	}
-	ns.print(`Navigate to the crims list to continue`)
+	ns.print(`Navigate to the crimes list to continue`)
 }
