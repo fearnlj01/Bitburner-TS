7f5108971062476c234a916eb9a96580e5a23d3e
Jordan Fearnley <fearnlj01@gmail.com>
2022-01-18 19:28:19 +0000

    making murder pretty again

--- src/TS/autoHomicide.ts
+++ src/TS/autoHomicide.ts
@@ -6,13 +6,16 @@ export async function main(ns : NS) : Promise<void> {
 	ns.tail()
 	if (globalThis['document'].querySelector('button:nth-child(8)')) {
 		const homicide = globalThis['document'].querySelector('button:nth-child(8)')
+		let counter = 0;
 		while(true) {
 			// @ts-expect-error placeholderDescription
 			ns.clearLog()
-			ns.print(ns.heart.break() * -1)
+			const karma = ns.heart.break() * -1
+			ns.print(`INFO\n${++counter} Victims\nKaram: ${karma}\n${Math.ceil((46e3 - karma)/3)} more victims required`)
 			homicide[Object.keys(homicide)[1]].onClick({ isTrusted : true })
-			await sleep(4005)
+			await sleep(3000)
 		}
 	}
+	
 	ns.print(`Navigate to the crimes list to continue`)
 }
