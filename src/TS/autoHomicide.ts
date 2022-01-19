9c8e3ff699dda725019238994a801cd01dc5cc12
Jordan Fearnley <fearnlj01@gmail.com>
2022-01-19 20:10:55 +0000

    minor tweaks to be less of a pain

--- src/TS/autoHomicide.ts
+++ src/TS/autoHomicide.ts
@@ -4,19 +4,21 @@ import { sleep } from '/TS/functions'
 export async function main(ns : NS) : Promise<void> {
 	ns.clearLog()
 	ns.tail()
-	if (globalThis['document'].querySelector('button:nth-child(8)')) {
-		const homicide = globalThis['document'].querySelector('button:nth-child(8)')
+	const query = 'button:nth-child(6)'
+	if (globalThis['document'].querySelector(query)) {
+		const homicide = globalThis['document'].querySelector(query)
 		let counter = 0;
 		while(true) {
 			ns.clearLog()
 			// @ts-expect-error placeholderDescription
 			const karma = ns.heart.break() * -1
-			ns.print(`INFO\n${++counter} Victims\nKaram: ${karma}\n${Math.ceil((46e3 - karma)/3)} more victims required`)
+			ns.print(`INFO\n${++counter} Victims\nKarma: ${karma}\n${Math.ceil((46e3 - karma)/3)} more victims required`)
 			// @ts-expect-error placeholderDescription
 			homicide[Object.keys(homicide)[1]].onClick({ isTrusted : true })
-			await sleep(3000)
+			await sleep(300e3)
 		}
 	}
+	
 	
 	ns.print(`Navigate to the crimes list to continue`)
 }
