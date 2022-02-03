e9129f62a66a97444466d9df73bece22c589fdd9
Jordan Fearnley <fearnlj01@gmail.com>
2022-02-03 00:53:20 +0000

    longer sleep, doesn't fill home memory

--- src/TS/xp.ts
+++ src/TS/xp.ts
@@ -13,12 +13,12 @@ export async function main(ns : NS) : Promise<void> {
 		const serverList = ['home', ...getHackableHosts(ns)]
 		for (const server of serverList) {
 			const serverData = ns.getServer(server)
-			const freeRam = serverData.maxRam - serverData.ramUsed
+			const freeRam = (server == 'home') ? ((serverData.maxRam * 0.8) - serverData.ramUsed) : serverData.maxRam - serverData.ramUsed
 			const threadCount = Math.floor(freeRam / 1.75)
 
 			await updateRemoteScripts(ns, server)
 			if (threadCount > 0) ns.exec(CONSTANTS.scripts.maxGrow, server, threadCount, CONSTANTS.xpServer, performance.now())
 		}
-		await sleep(5e2)
+		await sleep(10e3)
 	}
 }
