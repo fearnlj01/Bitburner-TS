666e371b2a848161ce40f0b6ed0f8a7277919d21
Jordan Fearnley <fearnlj01@gmail.com>
2022-02-03 01:22:28 +0000

    minor fixes and optimisations

--- src/TS/autoHomicide.ts
+++ src/TS/autoHomicide.ts
@@ -6,13 +6,6 @@ export async function main(ns : NS2) : Promise<void> {
 	ns.clearLog()
 	ns.tail()
 
-	if (ns.heart.break() <= -54e3) {
-		while (true) {
-			while (ns.isBusy()) await sleep(0)
-			await sleep(ns.commitCrime('Larceny'))
-		}
-	}
-
 	while (ns.heart.break() > -54e3) {
 		while (ns.isBusy()) await sleep(0)
 		await sleep(ns.commitCrime('homicide'))
@@ -20,9 +13,7 @@ export async function main(ns : NS2) : Promise<void> {
 		ns.print(`INFO\nKarma: ${ns.nFormat(-ns.heart.break(),'0.[0]')}\n${Math.ceil((54e3 + ns.heart.break())/3)} more victims required`)
 	}
 
-	ns.joinFaction('Slum Snakes')
-	if (ns.gang.inGang()) {
-		ns.gang.createGang('Slum Snakes')
-		ns.spawn('/TS/gang.js')
-	}
+	if (!(ns.getPlayer().factions.includes('Slum Snakes'))) ns.joinFaction('Slum Snakes') 
+	
+	ns.spawn('/TS/gang.js')
 }
